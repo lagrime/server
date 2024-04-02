671c0670380cd96f2e16eaf5c72e22b4263f6c86
elderguardian <katzenmaedchen@tutanota.com>
2024-04-02 14:43:28 +0200

    delete routes of unused controllers

--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,6 @@ $routes = [
     '/auth/complete' => [AuthCompleteController::class, 'index'],
 
     '/get/public-key' => [GetPublicKeyController::class, 'index', [AuthMiddleware::class]],
-    '/message/send' => [MessageSendController::class, 'index', [AuthMiddleware::class]],
-    '/message/receive' => [MessageReceiveController::class, 'index', [AuthMiddleware::class]],
 
     '/error' => function () {
         return 'Could not find this page.';
