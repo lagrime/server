1ab7298da2cd2795d830bd37e5eaa9497ab73483
elderguardian <katzenmaedchen@tutanota.com>
2024-04-04 20:52:55 +0200

    extend database accessor with set public key

--- src/components/database/contract/IDatabaseAccessor.php
+++ src/components/database/contract/IDatabaseAccessor.php
@@ -5,4 +5,6 @@ interface IDatabaseAccessor
     function getUserByName(string $user_name): User;
 
     function getUserByNameOrNull(string $user_name): ?User;
+
+    function setPublicKey(string $user_name, string $public_key): void;
 }
